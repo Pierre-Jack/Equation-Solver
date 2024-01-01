7500baf916efb5b4459acb7381125b1ac412fe2f
PierreJack <111060450+SantiagoMontag@users.noreply.github.com>
2024-01-02 01:54:30 +0200

    integrated 3 methods in main_2

--- main_2.py
+++ main_2.py
@@ -17,10 +17,12 @@ from sympy.parsing.sympy_parser import (
 from time import sleep
 from numpy import *
 import math
+import sys
+from sympy import *
 
 
 
-def bsct(u, l, tol, maxI):
+def bsct(u, l, tol, maxI, sig):
     if u < l:
         tmp=u
         u=l
@@ -35,13 +37,14 @@ def bsct(u, l, tol, maxI):
             l, u = r, u
         prev = r
         r = (l+u)/2
+        r = round(r, -int(math.floor(math.log10(abs(r)))) + (sig - 1))
         count = count+1
         if count > maxI:
-            return "max iterations exceeded"
+            return "max iterations exceeded", count
     print("num of iterations = " + str(count))
-    return r
+    return r, count
 
-def rf(u, l, tol, maxI):
+def rf(u, l, tol, maxI, sig):
     if u < l:
         tmp=u
         u=l
@@ -56,26 +59,155 @@ def rf(u, l, tol, maxI):
             l, u = r, u
         prev = r
         r = (l*f(u) - u*f(l))/(f(u)-f(l))
+        r = round(r, -int(math.floor(math.log10(abs(r)))) + (sig - 1))
         count = count+1
         if count > maxI:
-            return "max iterations exceeded"
+            return "max iterations exceeded", count
     print("num of iterations = " + str(count))
-    return r
+    return r, count
 
+def Fixed_Point(x0, eps, numOfIterations, g_function, significant_figures):
+    x_new = x0
+    k = 1
+    # g_function = lambda x: eval(g_function)
+    while (k<=numOfIterations) :
+        x_old = x_new
+        x_new = g_function(x_old)
+        x_new  = round(x_new, -int(math.floor(math.log10(abs(x_new)))) + (significant_figures - 1))
+        print(x_new)
+        if x_new == 0:
+            x_new = 1e-10
+        abs_error  = abs((x_new-x_old)/x_new)*100
+
+        if abs_error<=eps :
+            print(k)
+            print(x_new)
+            return x_new, k
+        k=k+1
+    if k == (numOfIterations + 1):
+        print(k)
+        print('Cannot Converge.')
+        return 'Cannot Converge.', k
+
+
+def g1(f, xi, m):
+    x = symbols('x')
+    d = diff(f, x)
+    if d == 0:
+        return "false"
+    return xi - m * (f.evalf(subs={'x': xi}) / float(d.evalf(subs={'x': xi})))
+
+
+def g2(f, xi):
+    x = symbols('x')
+    d1 = diff(f, x)
+    d2 = diff(d1, x)
+    a = f.evalf(subs={'x': xi})
+    b = d1.evalf(subs={'x': xi})
+    c = d2.evalf(subs={'x': xi})
+    if float((b**2)-a*c) == 0:
+        return "false"
+    return xi - (a*b)/float((b**2)-a*c)
+
+
+def modified1_newton_raphson_solver(f, x0, es, iter_max, n_sig, m):
+    x = symbols('x')
+    xip1 = x0
+    i = 0
+    ea = sys.maxsize
+    while i < iter_max and ea > es:
+        xi = xip1
+        xip1 = g1(f, xi, m)
+        if xip1 == "false":
+            return "Not Solvable by Modified 1 Newton Raphson", ea, i
+        if not xip1 == 0:
+            xip1 = round(xip1, -int(math.floor(math.log10(abs(xip1)))) + (n_sig - 1))
+        print(i, xip1)
+        if not (xip1 == 0):
+            ea = round(abs(((xip1 - xi) / xip1) * 100), 5)
+            print("ea of i =", i, "is equal ", ea)
+        i += 1
+    if i == iter_max and ea > es:
+        return "Solution is not found upon the given tolerance.", ea, i
+    print("ea final = ", ea)
+    return xip1, ea, i
+
+
+def modified2_newton_raphson_solver(f, x0, es, iter_max, n_sig):
+    x = symbols('x')
+    xip1 = x0
+    i = 0
+    ea = sys.maxsize
+    while i < iter_max and ea > es:
+        xi = xip1
+        xip1 = g2(f, xi)
+        if xip1 == "false":
+            return "Not Solvable by Modified 1 Newton Raphson", ea, i
+        if not xip1 == 0:
+            xip1 = round(xip1, -int(math.floor(math.log10(abs(xip1)))) + (n_sig - 1))
+        print(i, xip1)
+        if not (xip1 == 0):
+            ea = round(abs(((xip1 - xi) / xip1) * 100), 5)
+            print("ea of i =", i, "is equal ", ea)
+        i += 1
+    if i == iter_max and ea > es:
+        return "Solution is not found upon the given tolerance.", ea, i
+    print("ea final = ", ea)
+    if xip1 - int(xip1) < 10E-9:
+        xip1 = int(xip1)
+    return xip1, ea, i
+
+
+# x = symbols('x')
+# fx = x ** 3 - 5 * (x**2) + 7 * x - 3
+# print("Modified 1")
+# print(modified1_newton_raphson_solver(fx, 0, 0.00001, 50, 5, 2))
+# print("\nModified 2")
+# print(modified2_newton_raphson_solver(fx, 0, 0.00001, 50, 5))
+
+# x0 = 4
+# eps = 0.0001
+# numOfIterations = 50
+# significant_figures = 5
+# g_function_str = input("Enter the g(x) function as a Python expression, using 'x' as the variable: ")
+# g_function = g_function = lambda x: eval(g_function_str, {'x': x, 'e': math.e})
+# x = Fixed_Point(x0, eps, numOfIterations, g_function, significant_figures)
 
 
 def get_solution():
     l = float(guesses[0])
-    u = float(guesses[1])
+    try:
+        u = float(guesses[1])
+    except:
+        pass
+
+
 
     code = """def f(x):
                     return """ + func_f
     exec(code, globals())
 
+    if methodType == "Newton-Raphson(Modified-1)":
+        x = symbols('x')
+        fx = f(x)
+        s,ea,i = modified1_newton_raphson_solver(fx, l, eps, maxI, sig, 2)
+        return s, i
+
+
+    if methodType == "Newton-Raphson(Modified-2)":
+        x = symbols('x')
+        fx = f(x)
+        s,ea,i = modified2_newton_raphson_solver(fx, l, eps, maxI, sig)
+        return s, i
+
     if methodType == "Bisection":
-        return bsct(u, l, eps, maxI)
+        return bsct(u, l, eps, maxI, sig)
     if methodType == "False-Position":
-        return rf(u, l, eps, maxI)
+        return rf(u, l, eps, maxI, sig)
+    if methodType == "Fixed point":
+        g_function_str = func_g
+        g_function = lambda x: eval(g_function_str, {'x': x, 'e': math.e})
+        return Fixed_Point(l, eps, maxI, g_function, sig)
 
     return 1.5
 
@@ -115,16 +247,19 @@ def solve():
     global solution
 
     start_time = timer()
-    solution = get_solution()
+    solution, iterations = get_solution()
     end_time = timer()
     time_taken = round(((end_time - start_time) * 1000000), 5)
     print(time_taken)
 
-    new_label = Label(root, text="root" + ' = ' + str(solution))
+    new_label = Label(root, text="root" + ': ' + str(solution))
     new_label.grid(row=1, columnspan=2, sticky='W')
 
     new_label = Label(root, text="time taken: " + str(time_taken) + " µs.")
     new_label.grid(row=2, columnspan=2, sticky='W')
+
+    new_label = Label(root, text="iterations reached: " + str(iterations))
+    new_label.grid(row=3, columnspan=2, sticky='W')
 
 
 
